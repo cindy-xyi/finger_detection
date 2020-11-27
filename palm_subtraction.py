c153cff6ad11fcb107933222ddc49d4d58d76f46
Jessica Soong <jpsoong@gmail.com>
2020-11-27 12:54:10 -0800

    palm_subtraction and pre-processing data updated (val 86%)

--- palm_subtraction.py
+++ palm_subtraction.py
@@ -2,32 +2,40 @@
 import cv2
 import numpy as np
 import sys
-from scipy.signal import convolve2d
-from scipy.ndimage import gaussian_filter1d, gaussian_filter, maximum_filter
 import matplotlib.pyplot as plt
 from scipy import ndimage
 import util
 import os
 import p1n2 as hw
 
-def clean_image(depth_img):
+def convert2imgcoords(x, y, slope, num_row, scale=9999):
+    p1, p2 = (x, num_row - y), (x - scale, int(-scale * (-slope) + num_row - y))
+    if p2[0]*p2[1] > 0:
+        p1, p2 = (x, num_row - y),(x + scale, int(scale * (-slope) + num_row - y))
+    return p1, p2
 
-    # Binarize depth image
-    bin_img = util.binarize(depth_img, display=False)
-    bin_img[bin_img != 0] = 255
+def wrist_subtraction(palm_img, radius=30, display=False):
+    img = palm_img.copy()
+    attribute_list = hw.get_attribute(img/255)
+    h, w = img.shape
+    p1, p2 = convert2imgcoords(int((attribute_list[0]["position"]["x"])),
+                           int((attribute_list[0]["position"]["y"])),
+                           np.tan(attribute_list[0]["orientation"]), h)
+    p1 = (p1[0] + radius, p1[1])
+    p3 = (0, p1[1])
 
-    # Erode to get rid of noise
-    kernel = np.ones((10, 5))
-    bin_img = cv2.erode(bin_img, kernel)
+    corners = np.array([np.array(p1), np.array(p2), np.array(p3)])
 
-    # Ignore bottom few rows
-    bin_img[220:, :] = 0
+    return_img = cv2.fillPoly(img,[np.int32(corners)], 255)
+    if display is True:        
+        display_image = cv2.circle(return_img, p1, 2, 155, 2)
+        plt.imshow(display_image)
+    
+    return return_img
 
-    # Median filter to get rid of some more noise.
-    bin_img = ndimage.median_filter(bin_img, size=10)
 
+def subtract_smaller_areas(bin_img):
     _, lab_im = cv2.connectedComponents(bin_img)
-
 
     # find areas and subtract it
     max_area = -1
@@ -38,6 +46,23 @@ def clean_image(depth_img):
             max_area = area
             max_label = label
     bin_img = (lab_im == max_label).astype('uint8') * 255
+    return bin_img
+
+def clean_image(depth_img, kernel = np.ones((5, 5))):
+
+    # Binarize depth image
+    bin_img = util.binarize(depth_img, display=False)
+    
+    # Erode to get rid of noise
+    bin_img = cv2.erode(bin_img, kernel)
+
+    # Ignore bottom few rows
+    bin_img[220:, :] = 0
+    
+    bin_img = subtract_smaller_areas(bin_img)
+
+    # Median filter to get rid of some more noise.
+    bin_img = ndimage.median_filter(bin_img, size=10)
     
     return bin_img
 
@@ -53,18 +78,19 @@ def palm_subtraction(bin_img):
     kernel = cv2.circle(kernel, (20, 20), 20, (255, 255, 255), -1)
     image_dilate = cv2.dilate(image_erode, kernel[:, :, 1])
 
-    attribute_list = hw.get_attribute(image_dilate/255)
-    h, w = image_dilate.shape
-    radius = 25
+    image_dilate = wrist_subtraction(image_dilate)
+#     attribute_list = hw.get_attribute(image_dilate/255)
+#     h, w = image_dilate.shape
+#     radius = 25
 
-    y = np.arange(int(attribute_list[0]['position']['x']), h)
-    b = attribute_list[0]['position']['y'] - np.tan(attribute_list[0]['orientation'])*attribute_list[0]['position']['x']
-    m = np.tan(attribute_list[0]['orientation'])
+#     y = np.arange(int(attribute_list[0]['position']['x']), h)
+#     b = attribute_list[0]['position']['y'] - np.tan(attribute_list[0]['orientation'])*attribute_list[0]['position']['x']
+#     m = np.tan(attribute_list[0]['orientation'])
 
-    for (yi) in zip(y):
-        x_calc = int(np.round((yi - b)/ min(-m, m)))
-        # image_dilate[yi, x_calc - radius : x_calc + radius] = 255
-        image_dilate[yi, 0 : x_calc + radius] = 255
+#     for (yi) in zip(y):
+#         x_calc = int(np.round((yi - b)/ min(-m, m)))
+#         # image_dilate[yi, x_calc - radius : x_calc + radius] = 255
+#         image_dilate[yi, 0 : x_calc + radius] = 255
     
     # Subtract palm from hand to get fingers
     fingies = bin_img - image_dilate
